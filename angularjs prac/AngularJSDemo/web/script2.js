91419eae1486fe625255b9c4601228e3ca144dec
MYounas <younus.ubit56@gmail.com>
2016-09-13 10:49:23 +0500

    clean

--- angularjs prac/AngularJSDemo/web/script2.js
+++
@@ -1,8 +0,0 @@
-// Author: Suhaib Qaiser www.suhaibqaiser.com
-var app = angular.module("myApp", []);
-
-app.controller("TextCtrl", ["$scope", function($scope) {
-    $scope.textModel = {};
-    
-    $scope.textModel.value1 = "";
-}]);
